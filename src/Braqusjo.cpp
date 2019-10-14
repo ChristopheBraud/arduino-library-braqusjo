129bca4648ac51d18b6bc6b3fe1d1454c3809e40
Christophe Braud <christophe@agilitation.fr>
2019-10-14 22:11:01 +0200

    Initialize Library Structure

---
+++ src/Braqusjo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by christophe on 14/10/2019.
+//
+
+#include "Braqusjo.hpp"
---
+++ src/Braqusjo.hpp
@@ -0,0 +1,14 @@
+//
+// Created by christophe on 14/10/2019.
+//
+
+#ifndef BRAQUSJO_BRAQUSJO_HPP
+#define BRAQUSJO_BRAQUSJO_HPP
+
+
+class Braqusjo {
+
+};
+
+
+#endif //BRAQUSJO_BRAQUSJO_HPP
